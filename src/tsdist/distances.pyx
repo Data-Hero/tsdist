6190651d3e05c98fbbff0181e8611da1993ea0d1
riese <b.riesenberg@gmx.de>
2021-01-22 06:44:08 +0100

    initialized result variable

--- src/tsdist/distances.pyx
+++ src/tsdist/distances.pyx
@@ -4,8 +4,8 @@ from libc.math cimport sqrt, pow
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef double euclidean_distance(double[:] x, double[:] y):
-    cdef int i, length
-    cdef double result
+    cdef int i
+    cdef double result = 0.0
     for i in range(len(x)):
         result += pow(x[i]-y[i],2)
     return sqrt(result)
@@ -13,8 +13,8 @@ cpdef double euclidean_distance(double[:] x, double[:] y):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef double manhattan_distance(double[:] x, double[:] y):
-    cdef int i, length
-    cdef double result
+    cdef int i
+    cdef double result = 0.0
     for i in range(len(x)):
         result += abs(x[i]-y[i])
     return result
@@ -22,8 +22,8 @@ cpdef double manhattan_distance(double[:] x, double[:] y):
 @cython.boundscheck(False)
 @cython.wraparound(False)
 cpdef double minkowski_distance(double[:] x, double[:] y, int p):
-    cdef int i, length
-    cdef double result
+    cdef int i
+    cdef double result = 0.0
     for i in range(len(x)):
         result += pow(x[i]-y[i],p)
     return pow(result, 1/p)
--- test/test_simple.py
+++ test/test_simple.py
@@ -1,7 +1,7 @@
 import pyximport
 import numpy as np
 
-pyximport.install(setup_args={"script_args": ["--force"]})
+pyximport.install(setup_args={"script_args": ["--force"]}, language_level=3)
 import unittest
 
 from src.tsdist.distances import euclidean_distance
@@ -14,6 +14,8 @@ class TSDTest(unittest.TestCase):
         self.Y1 = np.array([0.0, -1.0, 1.0, -1.0, 0.0])
         self.X2 = np.array([0.2, 0.5, 0.1, 0.8, 1.2, 1.5])
         self.Y2 = np.array([0.1, 0.4, 0.05, 0.81, 1.25, 1.45])
+        self.X3 = np.repeat(1, 1000000)
+        self.Y3 = np.repeat(1, 1000000)
 
     def test_euclidean1(self):
         print(euclidean_distance(self.X1, self.Y1))
