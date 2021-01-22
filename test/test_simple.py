31e4c0f93aac0052923e702b43137edf4123dde8
riese <b.riesenberg@gmx.de>
2021-01-22 01:16:18 +0100

    second test works

--- test/test_simple.py
+++ test/test_simple.py
@@ -10,11 +10,18 @@ from src.tsdist.distances import euclidean_distance
 class TSDTest(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.X = np.array([1., -1., 1., -1., 0.])
-        self.Y = np.array([0., -1., 1., -1., 0.])
+        self.X1 = np.array([1.0, -1.0, 1.0, -1.0, 0.0])
+        self.Y1 = np.array([0.0, -1.0, 1.0, -1.0, 0.0])
+        self.X2 = np.array([0.2, 0.5, 0.1, 0.8, 1.2, 1.5])
+        self.Y2 = np.array([0.1, 0.4, 0.05, 0.81, 1.25, 1.45])
 
-    def test_euclidean(self):
-        self.assertAlmostEqual(euclidean_distance(self.X, self.Y), 1.0, places=5)
+    def test_euclidean1(self):
+        print(euclidean_distance(self.X1, self.Y1))
+        self.assertAlmostEqual(euclidean_distance(self.X1, self.Y1), 1.0, places=5)
+
+    def test_euclidean2(self):
+        print(euclidean_distance(self.X2, self.Y2))
+        self.assertAlmostEqual(euclidean_distance(self.X2, self.Y2), 0.1661325, places=5)
 
 
 if __name__ == "__main__":
